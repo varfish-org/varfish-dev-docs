7dd7d557d7ba3f126263659898fdd5d1c2b15a3c
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-02-29 10:00:06 +0100

    chore: config tweaks, version

--- src/conf.py
+++ src/conf.py
@@ -3,12 +3,24 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import subprocess
+
+# determine version from git
+git_version_bytes = subprocess.check_output(
+    ["git", "describe", "--tags", "--long"],
+    cwd=os.path.dirname(os.path.abspath(__file__)),
+)
+git_version = git_version_bytes.decode("utf-8").strip()[1:]
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "VarFish Dev Docs"
-copyright = "2024, Manuel Holtgrewe"
+project = "VarFish DevDocs"
+copyright = "2024, VarFish Team"
 author = "Manuel Holtgrewe"
+version = git_version
+release = git_version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,6 +33,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_title = "VarFish Dev Docs"
 html_theme = "furo"
 html_static_path = ["_static"]
 html_theme_options = {
